b0da954d8261fbca14214511d53d96f3a6811b80
Михаил Синяков <pvpshoot@gmail.com>
2016-02-18 18:36:33 +0300

    init repo

--- gulpCommonTasks/tasks/tasks.js
+++ gulpCommonTasks/tasks/tasks.js
@@ -1,6 +1,19 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
 
+
+gulp.task('browser-sync', function() {
+		browserSync.init({
+				server: {
+						baseDir: "./build"
+				}
+		});
+});
+
+gulp.task('reload', function(){
+	browserSync.watch("build/*.html").on("change", browserSync.reload);
+})
 // Default task
 gulp.task('default', function(cb) {
 	runSequence(
@@ -8,7 +21,9 @@ gulp.task('default', function(cb) {
 		'concatcss',
 		[ 'inliner', 'images'],
 		'watch',
+		'browser-sync',
+		'reload',
 		cb
 	);
-});
 
+});
--- gulpCommonTasks/tasks/watch.js
+++ gulpCommonTasks/tasks/watch.js
@@ -1,15 +1,20 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var config = require('../config');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task('watch', function () {
 	gulp.watch(config.concatcss.src, { cwd: config.concatcss.cwd }, ['build']);
 	gulp.watch(config.inliner.src, { cwd: config.inliner.cwd }, ['build']);
 	gulp.watch(config.inliner.src, { cwd: config.inliner.cwd + '/partials' }, ['build']);
 	gulp.watch(config.images.src, { cwd: config.images.cwd }, ['images']);
+	browserSync.watch("build/*.html").on("change", browserSync.reload);
 });
 
+
 gulp.task('build', function(cb) {
+
 	runSequence(
 		'concatcss',
 		'inliner',
