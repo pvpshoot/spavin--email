acbf539bc62b3c9edb1ec8463e2c706d1af47460
Михаил Синяков <pvpshoot@gmail.com>
2016-02-24 11:25:00 +0300

    bug fixes

--- gulpCommonTasks/tasks/tasks.js
+++ gulpCommonTasks/tasks/tasks.js
@@ -1,7 +1,24 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var email = require('gulp-email');
 var browserSync = require('browser-sync').create();
 
+
+// http://www.mailgun.com/
+var emailOptions = {
+		user: 'api:key-9f9095b5b2a2ba708349e55c64851683',
+		url: 'https://api.mailgun.net/v3/shoot.xcart.com/messages',
+		form: {
+				from: 'Excited User <mailgun@shoot.xcart.com>',
+				to: 'pvpshoot@gmail.com',
+				subject: 'The last dist'
+		}
+};
+
+gulp.task('sendemail', function () {
+		return gulp.src('build/registration-extended.html')
+				.pipe(email(emailOptions));
+});
 
 gulp.task('browser-sync', function() {
 		browserSync.init({
